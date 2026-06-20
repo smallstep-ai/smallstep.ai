50859c40acf800a8387f4e10402010df4af985df
Sagar Sarkale <sagarsarkale.work@gmail.com>
2026-06-20 15:12:05 +0530

    fix: use local General Sans fonts

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
 export const inter = Inter({
   subsets: ["latin"],
@@ -6,8 +7,19 @@ export const inter = Inter({
   display: "swap",
 });
 
-export const generalSans = Inter({
-  subsets: ["latin"],
+export const generalSans = localFont({
+  src: [
+    {
+      path: "../../public/fonts/GeneralSans-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/GeneralSans-Semibold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+  ],
   variable: "--font-general-sans",
   display: "swap",
 });
