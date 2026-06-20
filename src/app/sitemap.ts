cfcb347a3813b166b0311e978119db18acea0b32
Sagar Sarkale <sagarsarkale.work@gmail.com>
2026-06-20 15:17:06 +0530

    fix: tighten bootstrap sitemap metadata

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,8 +2,5 @@ import type { MetadataRoute } from "next";
 import { siteConfig } from "@/lib/site-config";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return ["", "/misal", "/making-misal", "/blog"].map((route) => ({
-    url: `${siteConfig.url}${route}`,
-    lastModified: new Date(),
-  }));
+  return [{ url: siteConfig.url }];
 }
